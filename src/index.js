d43acce665f92c5c1cb22b1792c48fdf65a37192
Svitlana Zaster <szaster@protonmail.com>
2020-12-03 19:55:25 -0600

    added basic elements and sign out buutton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const greeting = <div>Hello!</div>;
+function Header(props) {
+  return <h1>Hello {props.username}!</h1>;
+}
+
+function Layout(props) {
+  return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
+}
+// class Header extends React.Component {
+//   render() {
+//     return <h1>Hello!</h1>;
+//   }
+// }
+const year = 2020;
+const greeting = <div>Hello in {year}!</div>;
+// const isReactUser = false;
+function Signout() {
+  return <button>Signout</button>;
+}
+
 const rootNode = document.getElementById("root");
 
-ReactDOM.render(greeting, rootNode);
+function Login() {
+  return <p>Please login!</p>;
+}
+
+const isAuthenticated = true;
+
+ReactDOM.render(
+  <Layout>
+    {isAuthenticated ? <Header username="Jane" /> : <Login />}
+    {isAuthenticated && <Signout />}
+    <footer>Copyright 2020</footer>
+  </Layout>,
+  rootNode
+);
