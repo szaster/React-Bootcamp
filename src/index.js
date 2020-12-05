8cac6836c37bb1afdc1473e74d537c84f75ff323
Svitlana Zaster <szaster@protonmail.com>
2020-12-04 18:55:04 -0600

    added onClick, onChange event handlers

--- src/index.js
+++ src/index.js
@@ -17,30 +17,33 @@ function Layout(props) {
 const year = 2020;
 const greeting = <div>Hello in {year}!</div>;
 
-// const isReactUser = false;
-function Signout() {
-  return <button>Signout</button>;
-}
-
 const rootNode = document.getElementById("root");
-
-function Login() {
-  return <p>Please login!</p>;
-}
 
 const isAuthenticated = true;
 
-ReactDOM.render(
-  <Layout>
-    {isAuthenticated ? (
-      <React.Fragment>
-        <Header username="Jane" />
-        <Signout />
-      </React.Fragment>
-    ) : (
-      <Login />
-    )}
-    <footer>Copyright 2020</footer>
-  </Layout>,
-  rootNode
-);
+function App() {
+  const people = ["John", "Dave", "Jane"];
+
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+  return (
+    <ul>
+      {people.map((person, i) => (
+        <Person key={i} person={person} />
+      ))}
+      <input onChange={handleInputChange} />
+    </ul>
+  );
+}
+
+function Person(props) {
+  function handlePersonClick(event) {
+    alert(props.person);
+    console.log(event);
+  }
+  return <li onClick={handlePersonClick}>{props.person}</li>;
+}
+
+ReactDOM.render(<App />, rootNode);
