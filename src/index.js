c25af24101096010ac20156907d1acbac67d2447
Svitlana Zaster <szaster@protonmail.com>
2020-12-05 22:40:50 -0600

    implemented an object in useState

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import ReactDOM from "react-dom";
 const rootNode = document.getElementById("root");
 
 function App() {
-  const [language, setLanguage] = React.useState("python");
-  const [yearsExperience, setYearsExperience] = React.useState(0);
+  const [developer, setDeveloper] = React.useState({
+    language: "python",
+    yearsExperience: 0,
+  });
+  //   const [language, setLanguage] = React.useState("python");
+  //   const [yearsExperience, setYearsExperience] = React.useState(0);
   //   const [inputValue, setInputValue] = React.useState("");
   //   const inputValue = inputState[0];
   //   const setInputValue = inputState[1];
@@ -16,15 +20,29 @@ function App() {
   //   }
   return (
     <div>
-      <button onClick={() => setLanguage("Javascript")}>Change Language</button>
+      <button
+        onClick={() =>
+          setDeveloper({
+            language: "Javascript",
+            yearsExperience: 0,
+          })
+        }
+      >
+        Change Language
+      </button>
       <div>
         <input
           type="number"
-          onChange={(event) => setYearsExperience(event.target.value)}
+          onChange={(event) =>
+            setDeveloper({
+              ...developer,
+              yearsExperience: event.target.value,
+            })
+          }
         />
       </div>
-      <p>I am learning {language}</p>
-      <p>I have {yearsExperience} years of experience.</p>
+      <p>I am learning {developer.language}</p>
+      <p>I have {developer.yearsExperience} years of experience.</p>
       {/* // <input onChange={handleInputChange} />
       // <p>{inputValue}</p> */}
     </div>
