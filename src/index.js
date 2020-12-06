994cb5603c9221c613df2d444d3176f94b588781
Svitlana Zaster <szaster@protonmail.com>
2020-12-05 23:39:49 -0600

    added a toggler

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function App() {
   const [developer, setDeveloper] = React.useState({
     language: "python",
     yearsExperience: 0,
+    isEmployed: false,
   });
   //   const [language, setLanguage] = React.useState("python");
   //   const [yearsExperience, setYearsExperience] = React.useState(0);
@@ -18,31 +19,40 @@ function App() {
   //     setInputValue(event.target.value);
   //     console.log(inputValue);
   //   }
+
+  function handleChangeLanguage() {
+    setDeveloper({
+      language: "Javascript",
+      yearsExperience: 0,
+    });
+  }
+
+  function handleChangeYearsExperience(event) {
+    setDeveloper({
+      ...developer,
+      yearsExperience: event.target.value,
+    });
+  }
+
+  function handleToggleEmployment() {
+    setDeveloper((prevState) => ({
+      ...prevState,
+      isEmployed: !prevState.isEmployed,
+    }));
+  }
+
   return (
     <div>
-      <button
-        onClick={() =>
-          setDeveloper({
-            language: "Javascript",
-            yearsExperience: 0,
-          })
-        }
-      >
-        Change Language
-      </button>
+      <button onClick={handleToggleEmployment}>Toggle Employment Status</button>
+      <button onClick={handleChangeLanguage}>Change Language</button>
       <div>
-        <input
-          type="number"
-          onChange={(event) =>
-            setDeveloper({
-              ...developer,
-              yearsExperience: event.target.value,
-            })
-          }
-        />
+        <input type="number" onChange={handleChangeYearsExperience} />
       </div>
       <p>I am learning {developer.language}</p>
       <p>I have {developer.yearsExperience} years of experience.</p>
+      <p>
+        Employment Status: {developer.isEmployed ? "Employed" : "Unemployed"}
+      </p>
       {/* // <input onChange={handleInputChange} />
       // <p>{inputValue}</p> */}
     </div>
