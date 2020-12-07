24fe1c1412514d5c23281afa85112a257f8792ae
Svitlana Zaster <szaster@protonmail.com>
2020-12-06 22:51:10 -0600

    added useRef hook

--- src/index.js
+++ src/index.js
@@ -3,31 +3,49 @@ import ReactDOM from "react-dom";
 
 const rootNode = document.getElementById("root");
 
-const endpoint = "https://api.github.com/users/szaster";
+const baseUrl = "https://api.github.com/users/";
 
 function App() {
+  const [username, setUsername] = React.useState("szaster");
   const [user, setUser] = React.useState(null);
+  const searchInput = React.useRef();
 
   React.useEffect(() => {
-    async function getUser() {
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      setUser(data);
-    }
     getUser();
     // fetch(endpoint)
     //   .then((response) => response.json())
     //   .then((data) => setUser(data));
   }, []);
+  function handleClearInput() {
+    searchInput.current.value = "";
+    searchInput.current.focus();
+  }
+  async function getUser() {
+    const response = await fetch(`${baseUrl}${username}`);
+    const data = await response.json();
+    setUser(data);
+  }
 
-  return user ? (
+  return (
     <div>
-      <h2>{user.name}</h2>
-      <p>{user.bio} </p>
-      <img alt="avatar" src={user.avatar_url} style={{ height: 50 }} />
+      <input
+        type="text"
+        placeholder="Input username"
+        onChange={(event) => setUsername(event.target.value)}
+        ref={searchInput}
+      />
+      <button onClick={getUser}>Search</button>
+      <button onClick={handleClearInput}>Clear</button>
+      {user ? (
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.bio} </p>
+          <img alt="avatar" src={user.avatar_url} style={{ height: 50 }} />
+        </div>
+      ) : (
+        <p>loading........</p>
+      )}
     </div>
-  ) : (
-    <p>loading........</p>
   );
 }
 
