be8b98defde22658f40627b5cb8eae10770fed20
Svitlana Zaster <szaster@protonmail.com>
2020-12-06 19:29:04 -0600

    created a small program for tracking x,y mouse movements

--- src/index.js
+++ src/index.js
@@ -4,76 +4,32 @@ import ReactDOM from "react-dom";
 const rootNode = document.getElementById("root");
 
 function App() {
-  const [developer, setDeveloper] = React.useState({
-    name: "",
-    language: "python",
-    yearsExperience: 0,
-    isEmployed: false,
-  });
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+
   React.useEffect(() => {
-    document.title = developer.name;
-    console.log("runs");
-  }, [developer.name]);
-  //   const [language, setLanguage] = React.useState("python");
-  //   const [yearsExperience, setYearsExperience] = React.useState(0);
-  //   const [inputValue, setInputValue] = React.useState("");
-  //   const inputValue = inputState[0];
-  //   const setInputValue = inputState[1];
+    document.addEventListener("mousemove", handleMouseMove);
 
-  //   function handleInputChange(event) {
-  //     setInputValue(event.target.value);
-  //     console.log(inputValue);
-  //   }
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
-  function handleChangeLanguage() {
-    setDeveloper({
-      language: "Javascript",
-      yearsExperience: 0,
-    });
-  }
-
-  function handleChangeYearsExperience(event) {
-    setDeveloper({
-      ...developer,
-      yearsExperience: event.target.value,
-    });
-  }
-
-  function handleToggleEmployment() {
-    setDeveloper((prevState) => ({
-      ...prevState,
-      isEmployed: !prevState.isEmployed,
-    }));
-  }
-  function handleChangeName(event) {
-    setDeveloper({
-      ...developer,
-      name: event.target.value,
-    });
+  function handleMouseMove(event) {
+    setMousePosition({ x: event.pageX, y: event.pageY });
   }
   return (
     <div>
-      <button onClick={handleToggleEmployment}>Toggle Employment Status</button>
-      <button onClick={handleChangeLanguage}>Change Language</button>
-      <div>
-        <input type="number" onChange={handleChangeYearsExperience} />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={handleChangeName}
-          placeholder="Change name"
-        />
-      </div>
-      <p>I am learning {developer.language}</p>
-      <p>I have {developer.yearsExperience} years of experience.</p>
       <p>
-        Employment Status: {developer.isEmployed ? "Employed" : "Unemployed"}
+        {" "}
+        X: {mousePosition.x}, Y:{mousePosition.y}
       </p>
-      {/* // <input onChange={handleInputChange} />
-      // <p>{inputValue}</p> */}
     </div>
   );
 }
 
 ReactDOM.render(<App />, rootNode);
+
+function NewPage() {
+  return <div> new page</div>;
+}
+setTimeout(() => ReactDOM.render(<NewPage />, rootNode), 2000);
