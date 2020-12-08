43c1a3d6b7b201392f44c5acb8a61ee9a00fe126
Svitlana Zaster <szaster@protonmail.com>
2020-12-07 21:47:28 -0600

    Added choose file functionality to createPost component

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 function CreatePost() {
+  const [content, setContent] = React.useState("");
+  const [image, setImage] = React.useState(null);
   return (
     <div>
       <h1>Create New Post</h1>
       <form>
         <input type="text" placeholder="Add Post Content"></input>
-        <input type="file"></input>
+        <input
+          type="file"
+          onChange={(event) => setImage(event.target.files[0])}
+        ></input>
         <button type="submit">Submit Post</button>
       </form>
+      <p>{content}</p>
+      {image && (
+        <img
+          style={{ height: 200, width: 200, objectFit: "cover" }}
+          src={URL.createObjectURL(image)}
+        />
+      )}
     </div>
   );
 }
