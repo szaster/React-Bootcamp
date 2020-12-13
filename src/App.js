a2303d025ed71663af08ed04741a32e4a15568d0
Svitlana Zaster <szaster@protonmail.com>
2020-12-12 22:18:00 -0600

    added useReducer hook to manage posts states

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ import PostList from "./components/PostList";
 //const functionsCount = new Set();
 
 export const UserContext = React.createContext();
+export const PostContext = React.createContext({
+  posts: [],
+});
 
 function App() {
+  const initialPostState = React.useContext(PostContext);
+  const [state, dispatch] = React.useReducer(() => {}, initialPostState);
   const [user, setUser] = React.useState("");
-  const [posts, setPosts] = React.useState([]);
+  // const [posts, setPosts] = React.useState([]);
 
   //const [count, setCount] = React.useState(0);
 
@@ -20,12 +25,12 @@ function App() {
   }, [user]);
 
   //second callback tells when to execute this function
-  const handleAddPost = React.useCallback(
-    (newPost) => {
-      setPosts([newPost, ...posts]);
-    },
-    [posts]
-  );
+  // const handleAddPost = React.useCallback(
+  //   (newPost) => {
+  //     setPosts([newPost, ...posts]);
+  //   },
+  //   [posts]
+  // );
 
   // functionsCount.add(handleAddPost);
   //console logs # of functions stored here. Prevents
@@ -35,13 +40,16 @@ function App() {
     return <Login setUser={setUser} />;
   }
   return (
+    <PostContext.Provider value = {{state, dispatch}}>
     <UserContext.Provider value={user}>
       <Header user={user} setUser={setUser} />
       {/* <CreatePost user={user} setPosts={setPosts} posts={posts} /> */}
-      <CreatePost user={user} handleAddPost={handleAddPost} />
+      <CreatePost user={user}
+      //  handleAddPost={handleAddPost} />
       <PostList posts={posts} />
       {/* <button onClick={() => setCount((prev) => prev + 1)}>{count}+</button> */}
     </UserContext.Provider>
+    </PostContext.Provider>
   );
 }
 export default App;
